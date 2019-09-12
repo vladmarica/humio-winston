d5c8a0677094b00fe899e604e6e6e168c4ccac79
Quintinity <vlad.marica1@gmail.com>
2019-09-12 00:57:13 -0300

    Finish initial version of the transport

--- src/humio-winston.ts
+++ src/humio-winston.ts
@@ -1,20 +1,53 @@
-import Transport from 'winston-transport';
+import Transport, { TransportStreamOptions } from 'winston-transport';
+import fetch from 'node-fetch';
 
-export interface HumioTransportOptions extends Transport.TransportStreamOptions{
-    injestKey: string;
+const API_BASE_URL = 'https://cloud.humio.com';
+const UNSTRUCTURE_API_ENDPOINT = '/api/v1/ingest/humio-unstructured';
+
+export interface HumioTransportOptions extends TransportStreamOptions {
+    injestToken: string;
+    suppressErrors?: boolean; // If false, all errors are printed to stderr
+    tags?: { [key: string]: string }
 }
 
+const defaultOptions: Partial<HumioTransportOptions> = {
+    suppressErrors: true,
+};
+
 export default class HumioTransport extends Transport {
-    private injestKey: string;
+    private options: HumioTransportOptions;
 
     constructor(options: HumioTransportOptions) {
         super(options);
-        this.injestKey = options.injestKey;    
+        this.options = Object.assign({}, defaultOptions, options);
     }
 
     public log(info: any, callback: () => void): any {
         setImmediate(() => this.emit('logged', info));
 
+        const body: any = {
+            messages: [JSON.stringify(info)],
+        };
+
+        // Add tags to the request if they are given
+        if (this.options.tags) {
+            body.tags = this.options.tags;
+        }
+        this.sendInjestRequest(UNSTRUCTURE_API_ENDPOINT, [body]);
+
         callback();
+    }
+
+    private sendInjestRequest(endpoint: string, requestBody: any) {
+        fetch(API_BASE_URL + endpoint, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + this.options.injestToken,
+            },
+            body: JSON.stringify(requestBody)
+        })
+        .catch(err => !this.options.suppressErrors
+            && console.error('Failed to send log to Humio: ' + JSON.stringify(err)));
     }
 }
